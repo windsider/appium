dd65885506ef961cf9f50d59e5ddc2009670f75e
Andrey Grechukhin <vrzherdev@gmail.com>
2018-03-07 19:41:38 +0300

    appium tests

--- tests_js/client.js
+++ tests_js/client.js
@@ -10,7 +10,7 @@ function client() {
             platformName: "Android",
             platformVersion: "8.1.0",
             deviceName: "Android Emulator",
-            app: "C:/Users/ruvzherdev/Desktop/Appium/ApiDemos-debug.apk",
+            app: "C:/Appium/ApiDemos-debug.apk",
             automationName: "UiAutomator2"
         }
     }
