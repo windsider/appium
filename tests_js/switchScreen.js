e2758f1882bff717ed90ff91e96cdf57aec2c9dc
Andrey Grechukhin <vrzherdev@gmail.com>
2018-03-13 20:58:49 +0300

    modified switchScreen

--- tests_js/switchScreen.js
+++ tests_js/switchScreen.js
@@ -2,18 +2,20 @@ var client = require('./client');
 var Screen = require('./screenClass');
 var appItem = '~App';
 var devadminItem = "~Device Admin";
-var encr = '/hierarchy/android.widget.FrameLayout/android.view.ViewGroup/android.widget.FrameLayout[1]/android.view.ViewGroup';
-var initScn;
-var secScn;
+var encr = "android.widget.FrameLayout";
 
 var initialScreen = new Screen(appItem, client);
-var secondScreen = new Screen(devadminItem, client);
-
-
 initialScreen.isInitialized(appItem, client);
-initScn = initialScreen.submitScreen(appItem, client);
 
-secondScreen.isInitialized(devadminItem, initScn);
-secScn = secondScreen.submitScreen(devadminItem, initScn);
+var secondScreen = initialScreen.submitScreen(appItem, client);
+initialScreen.isInitialized(devadminItem, secondScreen);
 
-initialScreen.finalAssertion(encr, secScn);
+finalScreen = initialScreen.submitScreen(devadminItem, secondScreen);
+initialScreen.finalAssertion(encr, finalScreen);
+
+
+
+
+
+
+
